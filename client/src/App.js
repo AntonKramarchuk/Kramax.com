cd06c5fb58330d1b90b792e08db58a46cabc3500
Антон Крамарчук <antkramarchuk@gmail.com>
2023-05-31 22:37:21 +0300

    Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import Example from "./components/Example/Example";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Example/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route></Route>
+        </Route>
+      </Routes>
       <Example></Example>
     </div>
   );
