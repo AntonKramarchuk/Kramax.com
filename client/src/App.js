862802f0ac2ef07ef0f5124645d58d343808bb93
Vitalii <vexavv2022@gmail.com>
2023-06-01 00:02:13 +0300

    start footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import Example from "./components/Example/Example";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Test from './components/Test/Test'
+import Test from './components/Test/Test';
 function App() {
   return (
     <div className="App">
