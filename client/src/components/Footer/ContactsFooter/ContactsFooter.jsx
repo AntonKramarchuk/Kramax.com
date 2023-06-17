ddca0d706d93c3f33195ff951bcdc074fcfbe682
Vitalii <vexavv2022@gmail.com>
2023-06-17 13:01:47 +0300

    styled in MUI

--- client/src/components/Footer/ContactsFooter/ContactsFooter.jsx
+++ client/src/components/Footer/ContactsFooter/ContactsFooter.jsx
@@ -5,70 +5,68 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import {format} from 'date-fns'
-import{Link as RouterLink } from 'react-router-dom'
+import {Link as RouterLink} from 'react-router-dom'
+import {styled} from '@mui/material/styles';
+
+const StyledTypography = styled(Typography)(() => ({
+    color: '#FFF',
+    fontSize: '14px',
+    fontWeight: '200',
+    paddingBottom: '15px',
+}));
+const HoverStyledTypography = styled(StyledTypography)(() => ({
+    '&:hover': {
+        textDecoration: 'underline',
+    },
+}));
+
 function ContactsFooter() {
     return (
         <Box component='div' sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
             <Typography variant="body1" component='span' sx={{
                 color: '#FFF',
-                fontFamily: 'Montserrat, sans-serif',
                 fontSize: '20px',
                 fontWeight: '600',
                 lineHeight: "24px",
                 "&:hover": {textDecoration: 'underline'}
             }}>
-                <MaterialUILink href="tel:+38 (066) 000 00 00" underline="none" color="inherit">+38 (066) 000 00 00</MaterialUILink>
+                <MaterialUILink href="tel:+38 (066) 000 00 00" underline="none" color="inherit">+38 (066) 000 00
+                    00</MaterialUILink>
             </Typography>
             <Typography variant="body1" component='span' m={1} sx={{
                 color: '#DFB48B',
-                fontFamily: 'Montserrat, sans-serif',
                 fontSize: '14px',
                 fontWeight: '400',
                 lineHeight: "17px",
                 "&:hover": {textDecoration: 'underline'}
-            }}><MaterialUILink href="mailto:someNew@gmail.com" underline="none" color="inherit">someNew@gmail.com</MaterialUILink>
+            }}><MaterialUILink href="mailto:someNew@gmail.com" underline="none"
+                               color="inherit">someNew@gmail.com</MaterialUILink>
             </Typography>
             <List sx={{width: '100%', padding: '40px 0 50px 0'}}>
                 <ListItemIcon sx={{color: '#b9a897', display: 'flex', justifyContent: 'space-between'}}>
-                   <MaterialUILink href='https://facebook.com/' color="inherit" target="_blank" rel="noopener noreferrer"><FacebookOutlinedIcon sx={{ fontSize: 40, '&:hover': {color: '#DFB48B'}}}/></MaterialUILink>
-                    <MaterialUILink href='https://www.twitter.com/' color="inherit" target="_blank" rel="noopener noreferrer"><TwitterIcon sx={{ fontSize: 40, '&:hover': {color: '#DFB48B'}}}/></MaterialUILink>
-                    <MaterialUILink href='https://www.instagram.com/' color="inherit"target="_blank" rel="noopener noreferrer" ><InstagramIcon sx={{ fontSize: 40, '&:hover': {color: '#DFB48B'}}}/></MaterialUILink>
-                    <MaterialUILink href='https://www.youtube.com/' color="inherit" target="_blank" rel="noopener noreferrer"><YouTubeIcon sx={{ fontSize: 40, '&:hover': {color: '#DFB48B'}}}/></MaterialUILink>
+                    <MaterialUILink href='https://facebook.com/' color="inherit" target="_blank"
+                                    rel="noopener noreferrer"><FacebookOutlinedIcon/></MaterialUILink>
+                    <MaterialUILink href='https://www.twitter.com/' color="inherit" target="_blank"
+                                    rel="noopener noreferrer"><TwitterIcon/></MaterialUILink>
+                    <MaterialUILink href='https://www.instagram.com/' color="inherit" target="_blank"
+                                    rel="noopener noreferrer"><InstagramIcon/></MaterialUILink>
+                    <MaterialUILink href='https://www.youtube.com/' color="inherit" target="_blank"
+                                    rel="noopener noreferrer"><YouTubeIcon/></MaterialUILink>
                 </ListItemIcon>
             </List>
-            <Box>
-                <Typography component='p' sx={{
-                    color: '#FFF',
-                    fontFamily: 'Montserrat, sans-serif',
-                    fontSize: '14px',
-                    fontWeight: '200',
-                    paddingBottom: '15px',
-                }}>
+            <Box component='div'>
+                <StyledTypography component='p'>
                     © RemontProfi – будівельно-ремонтна <br/>компанія у Києві, 2008 - {format(new Date(), 'yyyy')}
-                </Typography>
-                <Typography component='p' sx={{
-                    color: '#FFF',
-                    fontFamily: 'Montserrat, sans-serif',
-                    fontSize: '14px',
-                    fontWeight: '200',
-                    paddingBottom: '15px',
-                    '&:hover':{textDecoration:'underline'}
-                }}>
-                    <MaterialUILink href='#' color="inherit" underline="none" target="_blank" rel="noopener noreferrer">Розробка та маркетинг- WE</MaterialUILink>
-                </Typography>
-                <Typography component='p' sx={{
-                    color: '#FFF',
-                    fontFamily: 'Montserrat, sans-serif',
-                    fontSize: '14px',
-                    fontWeight: '200',
-                    '&:hover':{textDecoration:'underline'}
-                }}>
-                   <MaterialUILink component={RouterLink}  target="_blank" to='privacy' underline="none" color="inherit">Політика конфіденційності </MaterialUILink>
-                </Typography>
-
+                </StyledTypography>
+                <HoverStyledTypography component='p'>
+                    <MaterialUILink href='#' color="inherit" underline="none" target="_blank" rel="noopener noreferrer">Розробка
+                        та маркетинг- WE</MaterialUILink>
+                </HoverStyledTypography>
+                <HoverStyledTypography component='p'>
+                    <MaterialUILink component={RouterLink} target="_blank" to='privacy' underline="none"
+                                    color="inherit">Політика конфіденційності </MaterialUILink>
+                </HoverStyledTypography>
             </Box>
-
-
         </Box>
 
     );
--- client/src/theme/Theme.jsx
+++ client/src/theme/Theme.jsx
@@ -1,16 +1,27 @@
-import { createTheme  } from "@mui/material";
-export const theme = createTheme({
-    palette:{
-        typography:{
-            fontFamily: 'Montserrat',
+import {createTheme} from "@mui/material";
 
+export const theme = createTheme({
+    typography: {
+        fontFamily: 'Montserrat,sans-serif ',
+    },
+    components: {
+        MuiSvgIcon: {
+            styleOverrides: {
+                root: {
+                    fontSize: 40,
+                    '&:hover': {color: '#DFB48B'}
+                },
+            },
         },
+    },
+    palette: {
+
         // primary:{
         //     50:'#2D404C',
         // },
-background:{
-  default: '#2D404C'  ,
-},
+        background: {
+            default: '#2D404C',
+        },
         text: {
             primary: '#C8DBE7',
             secondary: '#DDB38B',
