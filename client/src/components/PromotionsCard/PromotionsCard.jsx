8146efb3272631e4567f97d78c8cc23e94e9c7d8
Vitalii <vexavv2022@gmail.com>
2023-07-10 12:37:35 +0300

    added rows

--- client/src/components/PromotionsCard/PromotionsCard.jsx
+++ client/src/components/PromotionsCard/PromotionsCard.jsx
@@ -8,26 +8,41 @@ const styles = {
         textDecoration: 'none',
     },
 };
+
 function PromotionsCard({imageUrl, title, description, customId}) {
     const backgroundColor = customId % 2 === 0 ? '#DFB48B' : '#92C2ED';
     return (
-        <Card >
-            <CardMedia sx={{width:'350px', height:'490px',position:'relative'}}
+        <Card>
+            <CardMedia sx={{width: '350px', height: '490px', position: 'relative'}}
                        image={imageUrl}
                        alt="image">
-                <CardContent sx={{background:backgroundColor,position:'absolute', top:'25%',right:'0',width:'70%',minHeight:'200px'}}>
-                    <Link to='/' style={styles.link}><Typography sx={{fontSize: '30px',fontWeight:'200', color:'#172127',paddingBottom:'15px','&:hover': {textDecoration: 'underline'}}}>{title}</Typography></Link>
-                    <Typography sx={{fontSize: '16px',fontWeight:'200', color:'#1C272E'}}>{description}</Typography>
+                <CardContent sx={{
+                    background: backgroundColor,
+                    position: 'absolute',
+                    top: '25%',
+                    right: '0',
+                    width: '70%',
+                    minHeight: '200px'
+                }}>
+                    <Link to='/' style={styles.link}><Typography sx={{
+                        fontSize: '30px',
+                        fontWeight: '200',
+                        color: '#172127',
+                        paddingBottom: '15px',
+                        '&:hover': {textDecoration: 'underline'}
+                    }}>{title}</Typography></Link>
+                    <Typography sx={{fontSize: '16px', fontWeight: '200', color: '#1C272E'}}>{description}</Typography>
                 </CardContent>
-
+            
             </CardMedia>
         </Card>
     );
 }
+
 PromotionsCard.propTypes = {
     imageUrl: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    customId:PropTypes.string.isRequired
+    customId: PropTypes.string.isRequired
 };
 export default PromotionsCard;
--- client/src/components/PromotionsCarousel/PromotionsCarousel.jsx
+++ client/src/components/PromotionsCarousel/PromotionsCarousel.jsx
@@ -1,13 +1,15 @@
 import React, {useEffect} from 'react';
-import {Box, Container, IconButton, Typography, useMediaQuery} from "@mui/material";
+import {Box, Container,  Typography, useMediaQuery} from "@mui/material";
 import Carousel from "react-multi-carousel";
 import 'react-multi-carousel/lib/styles.css';
 import PropTypes from 'prop-types';
-import {ChevronLeft, ChevronRight} from "@mui/icons-material";
+// import {/*ChevronLeft,*/ ChevronRight} from "@mui/icons-material";
+import WestOutlinedIcon from '@mui/icons-material/WestOutlined';
 import {useDispatch, useSelector} from "react-redux";
 import {fetchAsyncPromotions} from "../../store/slices/promotionsSlace";
 import Error from '../Error/Error'
 import PromotionsCard from "../PromotionsCard/PromotionsCard";
+
 const responsive = {
     superLargeDesktop: {
         // the naming can be any, depends on you.
@@ -27,62 +29,58 @@ const responsive = {
         items: 1
     }
 };
-const ButtonGroup = ({ next, previous, ...rest }) => {
-    const { carouselState: { currentSlide } } = rest;
-    const isMobile = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+const ButtonGroup = ({next, previous, ...rest}) => {
+    const {carouselState: {currentSlide}} = rest;
+    const isDesktop = useMediaQuery((theme) => theme.breakpoints.down('md'));
     return (
         <Box
             component='div'
             sx={{
-                position: 'absolute',
-                bottom: isMobile ? 0 : 'auto',
-                left: isMobile ? '50%' : 0,
-                transform: isMobile ? 'translateX(-50%)' : 'none',
+                // position: isDesktop ? 'absolute': 'relative',
+                // bottom:isDesktop ? '10%' : 0,
+                // left: isDesktop ?'5%': 0,
+                // transform: isMobile ? 'translateX(-50%)' : 'none',
                 display: 'flex',
                 alignItems: 'center',
-                justifyContent: isMobile ? 'space-between' : 'flex-start',
-                padding: '0 16px',
-                width: isMobile ? '100%' : 'auto',
+                justifyContent: 'space-between',
+                padding: '10px 16px',
+                width: isDesktop ? '90%' : '100%',
             }}
         >
-            <IconButton
-                sx={{
-                    backgroundColor: 'transparent',
-                    border: 'none',
-                    outline: 'none',
-                    cursor: 'pointer',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: '#000',
-                    padding: '8px',
-                    margin: '0 8px',
-                    transition: 'opacity 0.3s',
-                    opacity: currentSlide === 0 ? 0.5 : 1,
-                    pointerEvents: currentSlide === 0 ? 'none' : 'auto',
-                }}
-                onClick={() => previous()}
-            >
-                <ChevronLeft />
-            </IconButton>
-            <IconButton
-                sx={{
-                    backgroundColor: 'transparent',
-                    border: 'none',
-                    outline: 'none',
-                    cursor: 'pointer',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: '#000',
-                    padding: '8px',
-                    margin: '0 8px',
-                    transition: 'opacity 0.3s',
-                }}
-                onClick={() => next()}
-            >
-                <ChevronRight />
-            </IconButton>
+            <Box component='div' sx={{
+                height: '70px',
+                width:'105px',
+                display: 'flex',
+                cursor: 'pointer',
+                position: 'relative',
+                opacity: currentSlide === 0 ? 0.5 : 1,
+                pointerEvents: currentSlide === 0 ? 'none' : 'auto'
+            }} onClick={() => previous()}>
+                <Box component='div' sx={{background: '#92C2ED', width: '70%', height: '100%'}}></Box>
+                    <WestOutlinedIcon  sx={{fontSize:'55px', position: 'absolute',
+                        top: '10%',
+                        left: '40%',
+                        fill:'#000'
+                   }}/>
+            
+            </Box>
+            <Box component='div' sx={{  height: '70px',
+                width:'105px',
+                display: 'flex',
+                flexDirection:'row-revers',
+                cursor: 'pointer',
+                position: 'relative',
+                transition: 'opacity 0.3s',
+            }} onClick={() => next()}>
+                <Box component='div' sx={{background: '#DFB48B', width: '70%', height: '100%'}}>
+                </Box>
+                <WestOutlinedIcon  sx={{fontSize:'55px', position: 'absolute',
+                    top: '10%',
+                    right: '40%',
+                    fill:'#000'
+                }}/>
+            </Box>
+            
         </Box>
     );
 };
@@ -90,42 +88,48 @@ const ButtonGroup = ({ next, previous, ...rest }) => {
 ButtonGroup.propTypes = {
     next: PropTypes.func,
     previous: PropTypes.func,
-
+    
 };
 
 
 function PromotionsCarousel() {
     const dispatch = useDispatch();
-    const {status, promotions, } = useSelector(state => state.promotions)
-
-
-    useEffect(()=>{
-        dispatch(fetchAsyncPromotions({ carousel: 'true' }))
-    },[dispatch])
-
-    switch(status){
+    const {status, promotions,} = useSelector(state => state.promotions)
+    
+    
+    useEffect(() => {
+        dispatch(fetchAsyncPromotions({carousel: 'true'}))
+    }, [dispatch])
+    
+    switch (status) {
         case 'loading':
             return <h2>Loading....</h2>;
         case 'loaded':
             return (
-                <Box sx={{paddingBottom:'15px'}}>
+                <Box sx={{paddingBottom: '15px'}}>
                     <Container>
-                        <Typography variant='h2' color='#5c6366' sx={{fontSize: {xs:'25px',sm:'30px',md:'50px'},fontWeight: '200', lineHeight: "1.2", marginBottom: '45px', letterSpacing:'3px'}}>Акції</Typography>
+                        <Typography variant='h2' color='#5c6366' sx={{
+                            fontSize: {xs: '25px', sm: '30px', md: '50px'},
+                            fontWeight: '200',
+                            lineHeight: "1.2",
+                            marginBottom: '45px',
+                            letterSpacing: '3px'
+                        }}>Акції</Typography>
                         <Carousel
                             responsive={responsive}
                             swipeable={true}
                             keyBoardControl={true}
                             arrows={false}
                             renderButtonGroupOutside={true}
-                            customButtonGroup={<ButtonGroup />}
+                            customButtonGroup={<ButtonGroup/>}
                             containerClass="carousel-container"
-
+                        
                         >
-                            {promotions.products && promotions.products.map(item=>{
+                            {promotions.products && promotions.products.map(item => {
                                 return <PromotionsCard key={item._id}  {...item}/>
-
+                                
                             })}
-
+                        
                         </Carousel>
                     </Container>
                 </Box>
@@ -133,8 +137,9 @@ function PromotionsCarousel() {
         default:
             return <Error error={status}/>
     }
-
+    
 }
+
 // PromotionsCarousel.propTypes = {
 //     onClick: PropTypes.func,
 // };
