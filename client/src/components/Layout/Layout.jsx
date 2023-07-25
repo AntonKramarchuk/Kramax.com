1007faec94ffb588bfc90813f4b233d1e2edb0d5
Vitalii <vexavv2022@gmail.com>
2023-07-25 18:31:10 +0300

    start

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import Footer from "../Footer/Footer";
+
 // import Header from "../Header/Header";
 
 
@@ -11,8 +12,9 @@ function Layout() {
             <main>
                 <Outlet/>
             </main>
-           <Footer/>
-            </>
+            
+            <Footer/>
+        </>
     );
 }
 
