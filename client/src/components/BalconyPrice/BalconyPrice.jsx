1b519ad6caaa9d700a6ff321654b3271c81b35a5
Vitalii <vexavv2022@gmail.com>
2023-08-04 22:56:38 +0300

    typografy

--- client/src/components/BalconyPrice/BalconyPrice.jsx
+++ client/src/components/BalconyPrice/BalconyPrice.jsx
@@ -4,16 +4,52 @@ import {Box, Container, Typography} from "@mui/material";
 const BalconyPrice = () => {
     return (
         <Container maxWidth="lg">
-           <Box component="div" sx={{width: '100%',height:'250px',padding:{xs:'40px 0',sm:'90px 0'}, position:'relative', display: 'flex', flexDirection:{xs:'column',sm:'row'}}}>
-               <Box component="div" sx={{width:{xs:'100%', sm:'70%'}, backgroundImage:`url(${"/image/balcony.jpg"})`, backgroundSize:'cover'}}>
-               <Typography></Typography>
-               </Box>
-               <Box component="div" sx={{width:{xs:'100%', sm:'30%'},display:{xs:'none',sm:'block'}, backgroundColor: '#c8dbe7'}}></Box>
-               {/*<Box component="div" sx={{position: 'absolute'}}></Box>*/}
-           </Box>
-            
-        </Container>
+            <Box component="div" sx={{
+                width: '100%',
+                marginBottom: '30px',
+                padding: {xs: '40px 0', sm: '90px 0'},
+                position: 'relative',
+                display: 'flex',
+                flexDirection: {xs: 'column', sm: 'row'}
+            }}>
+                <Box component="div" sx={{
+                    width: {xs: '100%', sm: '70%'},
+                    background: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${"/image/balcony.jpg"})`,
+                    backgroundSize: 'cover',
+                    padding: {xs: '30px 0', sm: '50px 0',md:'150px 0'}
+                }}>
+                    <Box component="div" sx={{
+                        width: {xs: '100%', sm: '60%'},
+                        height: '90%',
+                        margin: {xs: '10% auto', sm: '0'},
+                        paddingLeft: {sm: '10px', md: '15px'}
+                    }}>
+                        <Typography component='h3' color='#fff' sx={{
+                            fontSize: {xs: '24px', sm: '30px', md: '40px'},
+                            lineHeight: '1.2',
+                            textAlign: {xs: 'center', sm: 'left'},
+                            marginBottom: {xs: '15px', sm: '30px'}
+                        }}>Дізнайтесь вартість скління або оздоблення балкона </Typography>
+                        <Typography component='p' color='#fff' sx={{
+                            fontSize: {xs: '16px'},
+                            lineHeight: '1.9',
+                            textAlign: {xs: 'center', sm: 'left'}
+                        }}>Пройди тест та отримай розрахунок вартості ремонту свого балкона та приємний бонус – знижку
+                            12% та безкоштовний виїзд замірника </Typography>
+                    </Box>
+                
+                
+                </Box>
+                <Box component="div" sx={{
+                    width: {xs: '100%', sm: '30%'},
+                    display: {xs: 'none', sm: 'block'},
+                    backgroundColor: '#c8dbe7'
+                }}></Box>
+                {/*<Box component="div" sx={{position: 'absolute'}}></Box>*/}
+            </Box>
         
+        </Container>
+    
     );
 };
 
